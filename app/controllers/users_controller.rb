58fb5a2854be40aa005c6ddc5f93714a0a24e596
Jerome Sardoma <Jerome@jeromes-mbp.fios-router.home>
2015-12-16 12:43:33 -0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      #rails automatically infers from this that it should redirect to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      #we accept the params hash upon submission, require that the :user hash in params be valid
+      #input, and allow all keys of the :user hash
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
